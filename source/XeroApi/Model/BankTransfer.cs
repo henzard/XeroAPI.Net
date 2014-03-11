da78e0463b072f2071c8abbcf8eb63a2c7c24072
Matthew Steeples <matthew@mercuryit.co.uk>
2014-03-11 23:12:18 +0000

    Implement BankTransfer model

---
+++ source/XeroApi/Model/BankTransfer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XeroApi.Model
+{
+    public class BankTransfer : EndpointModelBase
+    {
+        [ItemId]
+        public Guid BankTransferID { get; set; }
+
+        [ItemUpdatedDate]
+        public DateTime? UpdatedDateUTC { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public Account FromAccount { get; set; }
+
+        public Account ToAccount { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [ReadOnly]
+        public Guid? BankTransferID { get; set; }
+
+        [ReadOnly]
+        public decimal? CurrencyRate { get; set; }
+
+        [ReadOnly]
+        public Guid? FromBankTransactionID { get; set; }
+
+        [ReadOnly]
+        public Guid? ToBankTransactionID { get; set; }
+    }
+}
