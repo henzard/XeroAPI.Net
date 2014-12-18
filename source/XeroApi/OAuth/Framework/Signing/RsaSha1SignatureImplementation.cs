7ae97f06646918f75d376fa4601030c6c5fda0a9
Matthew Steeples <matthew@mercuryit.co.uk>
2014-12-18 21:14:43 +0000

    Add missing using statements

--- source/XeroApi/OAuth/Framework/Signing/RsaSha1SignatureImplementation.cs
+++ source/XeroApi/OAuth/Framework/Signing/RsaSha1SignatureImplementation.cs
@@ -31,58 +31,62 @@ using System.Text;
 
 namespace DevDefined.OAuth.Framework.Signing
 {
-  public class RsaSha1SignatureImplementation : IContextSignatureImplementation
-  {
-    public string MethodName
+    public class RsaSha1SignatureImplementation : IContextSignatureImplementation
     {
-      get { return SignatureMethod.RsaSha1; }
+        public string MethodName
+        {
+            get { return SignatureMethod.RsaSha1; }
+        }
+
+        public void SignContext(IOAuthContext authContext, SigningContext signingContext)
+        {
+            authContext.Signature = GenerateSignature(authContext, signingContext);
+        }
+
+        public bool ValidateSignature(IOAuthContext authContext, SigningContext signingContext)
+        {
+            if (signingContext.Algorithm == null)
+                throw Error.AlgorithmPropertyNotSetOnSigningContext();
+
+            using (SHA1 sha1 = GenerateHash(signingContext))
+            {
+                var deformatter = new RSAPKCS1SignatureDeformatter(signingContext.Algorithm);
+                deformatter.SetHashAlgorithm("MD5");
+
+                byte[] signature = Convert.FromBase64String(authContext.Signature);
+
+                return deformatter.VerifySignature(sha1, signature);
+            }
+        }
+
+        string GenerateSignature(IOAuthContext authContext, SigningContext signingContext)
+        {
+            if (signingContext.Algorithm == null)
+                throw Error.AlgorithmPropertyNotSetOnSigningContext();
+
+            using (SHA1 sha1 = GenerateHash(signingContext))
+            {
+                var formatter = new RSAPKCS1SignatureFormatter(signingContext.Algorithm);
+                formatter.SetHashAlgorithm("MD5");
+                
+                byte[] signature = formatter.CreateSignature(sha1);
+
+                return Convert.ToBase64String(signature);
+            }
+        }
+
+        SHA1 GenerateHash(SigningContext signingContext)
+        {
+            var sha1 = SHA1.Create();// new SHA1Managed(); //CryptoServiceProvider();
+
+            byte[] dataBuffer = Encoding.ASCII.GetBytes(signingContext.SignatureBase);
+
+            using (var cs = new CryptoStream(Stream.Null, sha1, CryptoStreamMode.Write))
+            {
+                cs.Write(dataBuffer, 0, dataBuffer.Length);
+                cs.Close();
+            }
+            return sha1;
+        }
     }
-
-    public void SignContext(IOAuthContext authContext, SigningContext signingContext)
-    {
-      authContext.Signature = GenerateSignature(authContext, signingContext);
-    }
-
-    public bool ValidateSignature(IOAuthContext authContext, SigningContext signingContext)
-    {
-      if (signingContext.Algorithm == null) 
-          throw Error.AlgorithmPropertyNotSetOnSigningContext();
-
-      SHA1CryptoServiceProvider sha1 = GenerateHash(signingContext);
-
-      var deformatter = new RSAPKCS1SignatureDeformatter(signingContext.Algorithm);
-      deformatter.SetHashAlgorithm("MD5");
-
-      byte[] signature = Convert.FromBase64String(authContext.Signature);
-
-      return deformatter.VerifySignature(sha1, signature);
-    }
-
-    string GenerateSignature(IOAuthContext authContext, SigningContext signingContext)
-    {
-      if (signingContext.Algorithm == null) 
-          throw Error.AlgorithmPropertyNotSetOnSigningContext();
-
-      SHA1CryptoServiceProvider sha1 = GenerateHash(signingContext);
-
-      var formatter = new RSAPKCS1SignatureFormatter(signingContext.Algorithm);
-      formatter.SetHashAlgorithm("MD5");
-
-      byte[] signature = formatter.CreateSignature(sha1);
-
-      return Convert.ToBase64String(signature);
-    }
-
-    SHA1CryptoServiceProvider GenerateHash(SigningContext signingContext)
-    {
-      var sha1 = new SHA1CryptoServiceProvider();
-
-      byte[] dataBuffer = Encoding.ASCII.GetBytes(signingContext.SignatureBase);
-
-      var cs = new CryptoStream(Stream.Null, sha1, CryptoStreamMode.Write);
-      cs.Write(dataBuffer, 0, dataBuffer.Length);
-      cs.Close();
-      return sha1;
-    }
-  }
 }
